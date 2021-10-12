66ba86f798a0dde97e6211293b6ba652c476cfc9
YorkNishi999 <york.nishi@gmail.com>
2021-10-12 00:26:12 -0500

    fist

---
+++ dp_pt_1.py
@@ -0,0 +1,219 @@
+import numpy as np
+import torch
+import torch.optim as optim
+
+#### device ###
+def has_cuda():
+    return torch.cuda.is_available()
+
+def which_device():
+    return torch.device("cuda" if has_cuda() else "cpu")
+
+device = which_device()
+
+#### device ###
+
+print("Device is {}".format(device))
+
+torch.set_printoptions(edgeitems=2, linewidth=75)
+
+
+t_c = [0.5,  14.0, 15.0, 28.0, 11.0,  8.0,  3.0, -4.0,  6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
+t_c = torch.tensor(t_c).unsqueeze(1) # <1>
+t_u = torch.tensor(t_u).unsqueeze(1) # <1>
+
+t_u.shape
+
+n_samples = t_u.shape[0]
+n_val = int(0.2 * n_samples)
+
+shuffled_indices = torch.randperm(n_samples)
+
+train_indices = shuffled_indices[:-n_val]
+val_indices = shuffled_indices[-n_val:]
+
+train_indices, val_indices
+
+
+t_u_train = t_u[train_indices]
+t_c_train = t_c[train_indices]
+
+t_u_val = t_u[val_indices]
+t_c_val = t_c[val_indices]
+
+t_un_train = 0.1 * t_u_train
+t_un_val = 0.1 * t_u_val
+
+import torch.nn as nn
+
+linear_model = nn.Linear(1, 1) # <1>
+linear_model(t_un_val)
+
+linear_model.weight
+
+linear_model.bias
+
+
+x = torch.ones(1)
+linear_model(x)
+
+x = torch.ones(10, 1)
+linear_model(x)
+
+linear_model = nn.Linear(1, 1) # <1>
+optimizer = optim.SGD(
+    linear_model.parameters(), # <2>
+    lr=1e-2)
+
+linear_model.parameters()
+
+list(linear_model.parameters())
+
+
+def training_loop(n_epochs, optimizer, model, loss_fn, t_u_train, t_u_val,
+                  t_c_train, t_c_val):
+    for epoch in range(1, n_epochs + 1):
+        t_p_train = model(t_u_train) # <1>
+        loss_train = loss_fn(t_p_train, t_c_train)
+
+        t_p_val = model(t_u_val) # <1>
+        loss_val = loss_fn(t_p_val, t_c_val)
+        
+        optimizer.zero_grad()
+        loss_train.backward() # <2>
+        optimizer.step()
+
+        if epoch == 1 or epoch % 1000 == 0:
+            print(f"Epoch {epoch}, Training loss {loss_train.item():.4f},"
+                  f" Validation loss {loss_val.item():.4f}")
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c)**2
+    return squared_diffs.mean()
+
+linear_model = nn.Linear(1, 1) # <1>
+
+optimizer = optim.SGD(linear_model.parameters(), lr=1e-2)
+
+training_loop(
+    n_epochs = 3000, 
+    optimizer = optimizer,
+    model = linear_model,
+    loss_fn = loss_fn,
+    t_u_train = t_un_train,
+    t_u_val = t_un_val, 
+    t_c_train = t_c_train,
+    t_c_val = t_c_val)
+
+print()
+print(linear_model.weight)
+print(linear_model.bias)
+
+linear_model = nn.Linear(1, 1)
+optimizer = optim.SGD(linear_model.parameters(), lr=1e-2)
+
+training_loop(
+    n_epochs = 3000, 
+    optimizer = optimizer,
+    model = linear_model,
+    loss_fn = nn.MSELoss(), # <1>
+    t_u_train = t_un_train,
+    t_u_val = t_un_val, 
+    t_c_train = t_c_train,
+    t_c_val = t_c_val)
+
+print()
+print(linear_model.weight)
+print(linear_model.bias)
+
+seq_model = nn.Sequential(
+            nn.Linear(1, 13), # <1>
+            nn.Tanh(),
+            nn.Linear(13, 1)) # <2>
+seq_model
+
+[param.shape for param in seq_model.parameters()]
+
+for name, param in seq_model.named_parameters():
+    print(name, param.shape)
+
+from collections import OrderedDict
+
+seq_model = nn.Sequential(OrderedDict([
+    ('hidden_linear', nn.Linear(1, 8)),
+    ('hidden_activation', nn.Tanh()),
+    ('output_linear', nn.Linear(8, 1))
+]))
+
+seq_model
+
+for name, param in seq_model.named_parameters():
+    print(name, param.shape)
+
+seq_model.output_linear.bias
+
+optimizer = optim.SGD(seq_model.parameters(), lr=1e-3) # <1>
+
+training_loop(
+    n_epochs = 5000, 
+    optimizer = optimizer,
+    model = seq_model,
+    loss_fn = nn.MSELoss(),
+    t_u_train = t_un_train,
+    t_u_val = t_un_val, 
+    t_c_train = t_c_train,
+    t_c_val = t_c_val)
+    
+print('output', seq_model(t_un_val))
+print('answer', t_c_val)
+print('hidden', seq_model.hidden_linear.weight.grad)
+
+from matplotlib import pyplot as plt
+
+t_range = torch.arange(20., 90.).unsqueeze(1)
+
+fig = plt.figure(dpi=600)
+plt.xlabel("Fahrenheit")
+plt.ylabel("Celsius")
+plt.plot(t_u.numpy(), t_c.numpy(), 'o')
+plt.plot(t_range.numpy(), seq_model(0.1 * t_range).detach().numpy(), 'c-')
+plt.plot(t_u.numpy(), seq_model(0.1 * t_u).detach().numpy(), 'kx')
+plt.show()
+
+# Exercises here!
+
+neuron_count = 20
+
+seq_model = nn.Sequential(OrderedDict([
+    ('hidden_linear', nn.Linear(1, neuron_count)),
+    ('hidden_activation', nn.Tanh()),
+    ('output_linear', nn.Linear(neuron_count, 1))
+]))
+
+optimizer = optim.SGD(seq_model.parameters(), lr=1e-4)
+
+training_loop(
+    n_epochs = 5000,
+    optimizer = optimizer,
+    model = seq_model,
+    loss_fn = nn.MSELoss(),
+    t_u_train = t_un_train,
+    t_u_val = t_un_val,
+    t_c_train = t_c_train,
+    t_c_val = t_c_val)
+
+from matplotlib import pyplot as plt
+
+t_range = torch.arange(20., 90.).unsqueeze(1)
+
+fig = plt.figure(dpi=150)
+plt.xlabel("Fahrenheit")
+plt.ylabel("Celsius")
+plt.plot(t_u.numpy(), t_c.numpy(), 'o')
+plt.plot(t_range.numpy(), seq_model(0.1 * t_range).detach().numpy(), 'c-')
+plt.plot(t_u.numpy(), seq_model(0.1 * t_u).detach().numpy(), 'kx')
+plt.show()
+
+
+
